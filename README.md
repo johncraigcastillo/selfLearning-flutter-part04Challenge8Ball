ca934f0c1491ede7e7e58e5325db5d39e41208b5
johncastillo <johncraigcastillo@gmail.com>
2021-04-06 02:25:58 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Magic 8 Ball
+
+A challenge from part 4 of the [London App Brewery Flutter Tutorial](https://www.linkedin.com/learning/flutter-part-04-building-an-app-with-state/magic-8-ball-a-decision-making-app?u=74412284)
+
+
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Magic 8 Ball',
+      theme: ThemeData(
+        primaryColor: Colors.blue[800],
+        scaffoldBackgroundColor: Colors.blue[700],
+      ),
+      home: BallPage(),
+    );
+  }
+}
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int imageNum = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Magic 8 Ball'),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              child: Center(
+                child: TextButton(
+                  onPressed: () => randomNum(),
+                  child: Image.asset('images/ball$imageNum.png'),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  randomNum() => setState(() => imageNum = Random().nextInt(5) + 1);
+}
